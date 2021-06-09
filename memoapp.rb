06bc1825da157e17ce6228ac486fcad197b5c30b
EC2 Default User <ec2-user@ip-172-31-44-165.us-east-2.compute.internal>
2021-06-09 11:10:41 +0000

    test

--- memoapp.rb
+++ memoapp.rb
@@ -1,7 +1,22 @@
-    require "csv"
- 
-    puts "1(新規でメモを作成) 2(既存のメモ編集する)"
- 
-    memo_type = gets.to_s
- 
-    //続きを書いていきましょう！！(ifで条件分岐)
+require "csv"
+CSV.open('test.csv','w') do |test|
+    while true
+        puts "1(新規でメモを作成)、2(メモを編集する)、3(終了する)のいずれかをを入力してください。"
+        memo_type = gets.to_s
+    if memo_type == "1"
+      puts "拡張子を除いたファイルを入力していください。"
+    elsif memo_type == "2"
+      puts "編集内容を入力してください。"
+    elsif memo_type == "3"
+      puts "処理を終了します。"
+      break
+    else
+      puts "1または2または3を入力してください。"
+    end
+  end
+ end
+
+ require "csv"
+CSV.foreach("test.csv") do |row|
+  p row
+end
